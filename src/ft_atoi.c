5bdf56c892af8f501762405512fbcd2a20690ee2
Antoine Barthelemy <abarthel@student.42.fr>
2019-05-30 15:16:43 +0200

    atoi sipl

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 08:17:59 by abarthel          #+#    #+#             */
-/*   Updated: 2019/05/21 12:05:34 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/05/30 15:16:03 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ int		ft_atoi(const char *str)
 	i = 0;
 	nbr = 0;
 	sign = 1;
-	while (!(str[i] ^ ' ') || (str[i] > 8 && str[i] < 14))
+	while (str[i] == ' ' || (str[i] > 8 && str[i] < 14))
 		++i;
-	if (!(str[i] ^ '-') || !(str[i] ^ '+'))
+	if (str[i] == '-' || str[i] == '+')
 	{
-		sign = !(str[i] ^ '-') ? -1 : 1;
+		sign = str[i] == '-' ? -1 : 1;
 		++i;
 	}
 	while (str[i] > 47 && str[i] < 58)
 	{
 		val = str[i] ^ ((1 << 5) | (1 << 4));
 		if (nbr > nbr * 10 + val)
-			return (!(sign ^ -1) ? 0 : -1);
+			return (sign == -1 ? 0 : -1);
 		nbr = nbr * 10 + val;
 		++i;
 	}
