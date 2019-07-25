07a82d7c05b961483738fd98b985263c54372401
Antoine Barthelemy <abarthel@student.42.fr>
2019-07-25 10:27:58 -0400

    adding - GNU extension to getopt

--- tools/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-make -j -C ../ ;
-gcc main.c ../libft.a -I../include ;
-./a.out ;
-rm a.out ;
-gcc main1.c ../libft.a -I../include ;
-./a.out ;
-rm a.out ;
---
+++ tools/test_getopt/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+make -j -C ../../ ;
+gcc main.c ../../libft.a -I../../include ;
+./a.out ;
+rm a.out ;
+gcc main1.c ../../libft.a -I../../include ;
+./a.out ;
+rm a.out ;
