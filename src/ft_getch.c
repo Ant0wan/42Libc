d71c6bc41b636c92966818e6298fcbb2a3c24a53
Antoine Barthelemy <abarthel@student.42.fr>
2019-08-01 09:53:36 +0200

    change getch cdt

--- src/ft_getch.c
+++ src/ft_getch.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 18:36:04 by abarthel          #+#    #+#             */
-/*   Updated: 2019/07/11 18:38:15 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/08/01 09:53:15 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_getch(void)
 {
 	int	ch;
 
-	if ((int)read(STDIN_FILENO, (void*)&ch, sizeof(char)) == -1)
+	if ((int)read(STDIN_FILENO, (void*)&ch, sizeof(char)) <= 0)
 	{
 		return (-1);
 	}
