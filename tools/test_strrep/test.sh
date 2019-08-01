5d08aa258d0b7c2ba6bfb2be297a0ffe11c08b26
Antoine Barthelemy <abarthel@student.42.fr>
2019-08-01 17:02:19 +0200

    strrep for expansions sub

--- tools/test_strrep/test.sh
+++ tools/test_strrep/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 make -j -C ../../ ;
-#gcc -fsanitize=address main.c ../../libft.a -I../../include ;
-gcc main.c ../../libft.a -I../../include ;
+gcc -g -fsanitize=address main.c ../../libft.a -I../../include ;
+#gcc main.c ../../libft.a -I../../include ;
 ./a.out ;
-#rm a.out ;
+rm a.out ;
+rm -rf a.out.dSYM ;
