3d0c2c399b7a71a5186c289ebf7e6cc3ffd86e47
Antoine Barthelemy <abarthel@student.42.fr>
2019-08-25 09:37:44 -0400

    norm 42

--- tools/norme.sh
+++ tools/norme.sh
@@ -3,7 +3,6 @@
 ##
 ## This script changes some C keywords in .h and .c to make the lib 42 or ansi compliant
 ##
-cd ../
 if [ "$1" == "ansi" ]
 then
 	if [ $(grep -a __inline__ **/*.c | wc -l) == "0" ] && [ $(grep -a __inline__ **/*.h | wc -l) == "0" ]
@@ -53,4 +52,3 @@ then
 	)
 	printf "Restrict 42norm\n"
 fi
-cd -
