6f8b9096495177722e2b579da854ad8c9b22fd6d
Antoine Barthelemy <abarthel@student.42.fr>
2019-08-27 19:42:17 +0200

    norme 42 on the go

--- include/ft_strrep.h
+++ include/ft_strrep.h
@@ -1,8 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrep.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/27 19:40:57 by abarthel          #+#    #+#             */
+/*   Updated: 2019/08/27 19:41:55 by abarthel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef FT_STRREP_H
 # define FT_STRREP_H
 
-struct s_strrep
+struct			s_strrep
 {
 	const char	*content;
 	const char	*expansion;
