571fd4bb340a4670b70b68cf06fd6118db630aa6
Antoine Barthelemy <abarthel@student.42.fr>
2019-08-27 19:40:24 +0200

    norme 42 on the go

--- src/ft_fgetline.c
+++ src/ft_fgetline.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 16:48:52 by abarthel          #+#    #+#             */
-/*   Updated: 2019/07/15 15:26:01 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/08/27 19:40:05 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,34 +51,43 @@ static inline char	*create_new_line(char *str, int *len)
 	}
 }
 
-int					ft_fgetline(const int fd, char **line, char c)
+static inline int	inwhile_fgetline(const int fd, char **line,
+		char c, char *new_line)
 {
 	int		len;
 	int		ret;
-	char	tmp;
-	char	*new_line;
+	int		tmp;
 
 	len = 0;
 	ret = 0;
+	new_line = create_new_line(new_line, &len);
+	if (len == -1)
+		return (-1);
+	*line = new_line;
+	while (ret < len)
+	{
+		tmp = ft_fgetch(fd);
+		if (tmp == c || !tmp)
+		{
+			(*line)[ret] = 0;
+			return (ret);
+		}
+		(*line)[ret] = tmp;
+		++ret;
+	}
+	return (0);
+}
+
+int					ft_fgetline(const int fd, char **line, char c)
+{
+	int		ret;
+	char	*new_line;
+
 	new_line = NULL;
 	while (1)
 	{
-		len += 128;
-		new_line = create_new_line(new_line, &len);
-		if (len == -1)
-			return (-1);
-		*line = new_line;
-		while (ret < len)
-		{
-			tmp = ft_fgetch(fd);
-			if (tmp == c || !tmp)
-			{
-				(*line)[ret] = 0;
-				return (ret);
-			}
-			(*line)[ret] = tmp;
-			++ret;
-		}
+		if ((ret = inwhile_fgetline(fd, line, c, new_line)))
+			return (ret);
 	}
 	return (ret);
 }
