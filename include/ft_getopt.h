9c1b83c0b8b0b932ce411876423b432351004e56
Antoine Barthelemy <abarthel@student.42.fr>
2019-09-28 06:03:17 -0400

    fix(ft_getopt): add reset macro for g_optind reset between getopt call

--- include/ft_getopt.h
+++ include/ft_getopt.h
@@ -6,12 +6,18 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 13:49:41 by abarthel          #+#    #+#             */
-/*   Updated: 2019/07/25 16:43:26 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/09/25 16:32:10 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_GETOPT_H
 # define FT_GETOPT_H
+
+# ifdef __unix__
+#  define RESET_OPTIND 0
+# else
+#  define RESET_OPTIND 1
+# endif
 
 extern char	*g_optarg;
 extern int	g_optind;
