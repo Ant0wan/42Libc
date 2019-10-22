b412d4ade7ff8b2f45f3c8b5ed64071515ca89cd
Antoine Barthelemy <abarthel@student.42.fr>
2019-10-22 16:52:34 +0200

    adding2 stack data struct to libft

--- tools/test_stack/main.c
+++ tools/test_stack/main.c
@@ -21,9 +21,21 @@ int main(void)
 	str2 = strdup("Second line added to the stack\n");
 	stack_push(&mystack, (void*)str2);
 	printf("%s", (char*)stack_peek(&mystack));
+	char	*str3;
+	str3 = strdup("Third line into the stack\n");
+	stack_push(&mystack, (void*)str3);
+	printf("%s\n", (char*)stack_peek(&mystack));
+
+	stack_swap(&mystack);
+	printf("Here is the top after SWAP    %s", (char*)stack_peek(&mystack));
 
 	stack_pop(&mystack, free);
 	printf("%s", (char*)stack_peek(&mystack));
+	stack_swap(&mystack);
+	stack_pop(&mystack, free);
+	stack_swap(&mystack);
+	stack_pop(&mystack, free);
+	stack_swap(&mystack);
 	stack_pop(&mystack, free);
 	printf("isempty, should be 1: %d\n", (int)stack_isempty(&mystack));
 
