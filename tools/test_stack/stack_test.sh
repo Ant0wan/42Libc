998213bab392474b8faf64357b4a168f41195a6e
Antoine Barthelemy <abarthel@student.42.fr>
2019-10-25 09:07:36 -0400

    test fix

--- tools/test_stack/stack_test.sh
+++ tools/test_stack/stack_test.sh
@@ -12,7 +12,7 @@
 # **************************************************************************** #
 
 make -j -C ../../
-gcc main.c -fsanitize=address -g -Wextra -Wall -Werror ../../libft.a -I../../include -o test
+gcc main.c -fsanitize=address -g -Wextra -Wall ../../libft.a -I../../include -o test
 ./test
 rm test
 rm -rf test.dSYM
