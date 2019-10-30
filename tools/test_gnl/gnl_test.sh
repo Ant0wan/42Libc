57b95d645e697779f175b1721997916b35d6054d
Antoine Barthelemy <abarthel@student.42.fr>
2019-10-30 16:29:31 -0400

    gnl tests

---
+++ tools/test_gnl/gnl_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+make -j -C ../../
+#cc -g -Wall -Werror -Wextra -fsanitize=address -g main.c ../../libft.a -I../../include/ -o test
+cc -g -Wall -Werror -Wextra main.c ../../libft.a -I../../include/ -o test
+
+list=(`ls inputs`)
+for elem in ${list[@]}
+do
+	printf "Test name: $elem\n\nNumber of lines: "
+	time ./test inputs/$elem | wc -l
+	printf "\n-------------------------------\n"
+done
+rm test
