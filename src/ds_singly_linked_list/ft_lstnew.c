8d33e44d7ad10d0a4cabd4cb1a0da9b70d15ef2b
Antoine Barthelemy <abarthel@student.42.fr>
2019-11-06 16:23:52 +0100

    fix lstnew

--- src/ds_singly_linked_list/ft_lstnew.c
+++ src/ds_singly_linked_list/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 10:28:01 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/29 12:24:20 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/11/06 16:23:44 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,12 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*new;
 
-	new = (t_list*)malloc(sizeof(t_list));
+	new = (t_list*)ft_memmalloc(sizeof(t_list));
 	if (!new)
 		return (NULL);
 	if (!content)
-	{
-		new->content = NULL;
-		new->content_size = 0;
-		new->next = NULL;
 		return (new);
-	}
-	new->content = ft_strdup(content);
-	if (!new->content)
-	{
-		free(new);
-		return (NULL);
-	}
+	new->content = (void*)content;
 	new->content_size = content_size;
-	new->next = NULL;
 	return (new);
 }
