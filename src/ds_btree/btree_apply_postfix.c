5401e1ebea95d6636cfed09589bca2488a8f1aea
Antoine Barthelemy <abarthel@student.42.fr>
2020-05-19 19:29:14 +0200

    norm(libc): fixing norm on various files

--- src/ds_btree/btree_apply_postfix.c
+++ src/ds_btree/btree_apply_postfix.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 17:40:22 by abarthel          #+#    #+#             */
-/*   Updated: 2019/10/21 17:40:26 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/05/19 19:28:55 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 #include "ft_btree.h"
 
-void		btree_apply_postfix(struct s_btree *root, void (*applyf)(void *))
+void	btree_apply_postfix(struct s_btree *root, void (*applyf)(void *))
 {
 	if (root->left)
 		btree_apply_postfix(root->left, applyf);
